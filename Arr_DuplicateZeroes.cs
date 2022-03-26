a56459834a51270e2924ed26fb60235916157f14
Pallavi2525 <mail.singhpallavi@gmail.com>
2022-03-26 13:08:47 +0530

    LeetCode problems related to Arrays

---
+++ Arr_DuplicateZeroes.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public void DuplicateZeros(int[] arr) {
+        for(int i = arr.Length - 1; i >= 0; i--){
+            if(arr[i] == 0){
+                for(int j = arr.Length-1; j>i; j--){
+                    arr[j] = arr[j-1];
+                }
+            }
+        }
+    }
+}
