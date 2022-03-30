09d8cfa0b7e313c83d0969cc2763eabc0d80b325
Pallavi2525 <mail.singhpallavi@gmail.com>
2022-03-30 21:30:04 +0530

    1346. Check If N and Its Double Exist - LeetCode

---
+++ Arr_FindDoubleOfInteger.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool CheckIfExist(int[] arr) {
+        for(int i = 0; i < arr.Length; i++){
+            if(Array.Exists(arr, a => a == 2*arr[i]))
+            {
+                if(Array.IndexOf(arr, 2*arr[i]) != i)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
