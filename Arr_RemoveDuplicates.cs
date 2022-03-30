47d94c980f50985c089749a48c866a4c2e75ea97
Pallavi2525 <mail.singhpallavi@gmail.com>
2022-03-30 21:08:15 +0530

    26. Remove duplicates from sorted array - LeetCode

---
+++ Arr_RemoveDuplicates.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.Length; i++){
+            if(i< nums.Length -1 && nums[i] == nums[i+1])
+                continue;
+            nums[count++] = nums[i];
+        }
+        return count;
+    }
+}
