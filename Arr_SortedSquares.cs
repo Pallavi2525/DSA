2b27fea01fd480d3250d40f435815d05dcbf3e8e
Pallavi2525 <mail.singhpallavi@gmail.com>
2022-04-17 13:45:14 +0530

    Update - LeetCode #977 - Squares of a Sorted Array

--- Arr_SortedSquares.cs
+++ Arr_SortedSquares.cs
@@ -1,11 +1,24 @@
+//LeetCode - 977. Squares of a Sorted Array
+
 public class Solution {
     public int[] SortedSquares(int[] nums) {
-        List<int> arrResult = new List<int>();
-        foreach(int i in nums){
-            arrResult.Add(i*i);
+        int square = 0;
+        int n = nums.Length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+        for(int i = n - 1; i >= 0; i--){
+            if(Math.Abs(nums[left]) < Math.Abs(nums[right])){
+            square = nums[right];
+            right--;
         }
-        int[] result = arrResult.ToArray();
-        Array.Sort(result);
+        else{
+            square = nums[left];
+            left++;
+        }
+        square = square * square;
+        result[i] = square;
+        }
         return result;
     }
 }
